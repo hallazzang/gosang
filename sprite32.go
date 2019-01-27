d7406f9a866fbe080b76619d6e5a524acef9b3a0
Hanjun Kim <hallazzang@gmail.com>
2019-01-27 12:31:34 +0900

    add basic fields and implement basic methods; Width, Height, Count

--- sprite32.go
+++ sprite32.go
@@ -2,19 +2,22 @@ package gosang
 
 // Sprite32 is a 32-bit color sprite.
 type Sprite32 struct {
+	width  int
+	height int
+	count  int
 }
 
 // Width returns sprite's frame width, in pixel.
 func (sp *Sprite32) Width() int {
-	return 0
+	return sp.width
 }
 
 // Height returns sprite's frame height, in pixel.
 func (sp *Sprite32) Height() int {
-	return 0
+	return sp.height
 }
 
 // Count returns sprite's frame count.
 func (sp *Sprite32) Count() int {
-	return 0
+	return sp.count
 }
--- sprite8.go
+++ sprite8.go
@@ -2,19 +2,22 @@ package gosang
 
 // Sprite8 is an 8-bit color sprite.
 type Sprite8 struct {
+	width  int
+	height int
+	count  int
 }
 
 // Width returns sprite's frame width, in pixel.
 func (sp *Sprite8) Width() int {
-	return 0
+	return sp.width
 }
 
 // Height returns sprite's frame height, in pixel.
 func (sp *Sprite8) Height() int {
-	return 0
+	return sp.height
 }
 
 // Count returns sprite's frame count.
 func (sp *Sprite8) Count() int {
-	return 0
+	return sp.count
 }
