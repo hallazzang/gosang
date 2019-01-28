5663fed178a346459f7fdb79f06ba4b7802b49c1
Hanjun Kim <hallazzang@gmail.com>
2019-01-28 12:28:32 +0900

    test: make sure file to be closed

--- test/sprite_test.go
+++ test/sprite_test.go
@@ -15,26 +15,28 @@ func TestNewSprite(t *testing.T) {
 		{`data\arrow.spr`, 8, 20, 20, 10},
 		{`data\BUTTMENU_ONLINE_1.S32`, 32, 24, 52, 2},
 	} {
-		f, err := os.Open(tc.path)
-		if err != nil {
-			t.Fatalf("failed to open sprite file: %v", err)
-		}
-		sp, err := gosang.NewSprite(f)
-		if err != nil {
-			t.Fatalf("failed to open sprite: %v", err)
-		}
-		if cb := sp.ColorBits(); cb != tc.colorBits {
-			t.Errorf("bad sprite color bits; expected %d, got %d", tc.colorBits, cb)
-		}
-		if w := sp.Width(); w != tc.width {
-			t.Errorf("bad sprite frame width; expected %d, got %d", tc.width, w)
-		}
-		if h := sp.Height(); h != tc.height {
-			t.Errorf("bad sprite frame height; expected %d, got %d", tc.height, h)
-		}
-		if c := sp.Count(); c != tc.count {
-			t.Errorf("bad sprite frame count; expected %d, got %d", tc.count, c)
-		}
-		f.Close()
+		func() {
+			f, err := os.Open(tc.path)
+			if err != nil {
+				t.Fatalf("failed to open sprite file: %v", err)
+			}
+			defer f.Close()
+			sp, err := gosang.NewSprite(f)
+			if err != nil {
+				t.Fatalf("failed to open sprite: %v", err)
+			}
+			if cb := sp.ColorBits(); cb != tc.colorBits {
+				t.Errorf("bad sprite color bits; expected %d, got %d", tc.colorBits, cb)
+			}
+			if w := sp.Width(); w != tc.width {
+				t.Errorf("bad sprite frame width; expected %d, got %d", tc.width, w)
+			}
+			if h := sp.Height(); h != tc.height {
+				t.Errorf("bad sprite frame height; expected %d, got %d", tc.height, h)
+			}
+			if c := sp.Count(); c != tc.count {
+				t.Errorf("bad sprite frame count; expected %d, got %d", tc.count, c)
+			}
+		}()
 	}
 }
