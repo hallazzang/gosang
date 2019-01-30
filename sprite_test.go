0ec13130a3becf5647aaf5de21876607f441684d
Hanjun Kim <hallazzang@gmail.com>
2019-01-30 22:35:14 +0900

    test: print sprite's name if an error occurred

--- sprite_test.go
+++ sprite_test.go
@@ -23,59 +23,59 @@ func TestNewSprite(t *testing.T) {
 		func() {
 			f, err := os.Open(filepath.Join("test", "data", tc.name))
 			if err != nil {
-				t.Fatalf("failed to open sprite file: %v", err)
+				t.Fatalf("sprite %q: failed to open file: %v", tc.name, err)
 			}
 			defer f.Close()
 			sp, err := NewSprite(f)
 			if err != nil {
-				t.Fatalf("failed to open sprite: %v", err)
+				t.Fatalf("sprite %q: failed to open sprite: %v", tc.name, err)
 			}
 			if cb := sp.ColorBits(); cb != tc.colorBits {
-				t.Errorf("bad sprite color bits; expected %d, got %d", tc.colorBits, cb)
+				t.Errorf("sprite %q: bad color bits; expected %d, got %d", tc.name, tc.colorBits, cb)
 			}
 			if w := sp.FrameWidth(); w != tc.frameWidth {
-				t.Errorf("bad sprite frame width; expected %d, got %d", tc.frameWidth, w)
+				t.Errorf("sprite %q: bad frame width; expected %d, got %d", tc.name, tc.frameWidth, w)
 			}
 			if h := sp.FrameHeight(); h != tc.frameHeight {
-				t.Errorf("bad sprite frame height; expected %d, got %d", tc.frameHeight, h)
+				t.Errorf("sprite %q: bad frame height; expected %d, got %d", tc.name, tc.frameHeight, h)
 			}
 			if c := sp.FrameCount(); c != tc.frameCount {
-				t.Errorf("bad sprite frame count; expected %d, got %d", tc.frameCount, c)
+				t.Errorf("sprite %q: bad frame count; expected %d, got %d", tc.name, tc.frameCount, c)
 			}
 			if w := sp.Width(); w != tc.width {
-				t.Errorf("bad sprite width; expected %d, got %d", tc.width, w)
+				t.Errorf("sprite %q: bad width; expected %d, got %d", tc.name, tc.width, w)
 			}
 			if h := sp.Height(); h != tc.height {
-				t.Errorf("bad sprite height; expected %d, got %d", tc.height, h)
+				t.Errorf("sprite %q: bad height; expected %d, got %d", tc.name, tc.height, h)
 			}
 		}()
 	}
 }
 
 func TestFrameSizeAndOffset(t *testing.T) {
-	for _, tc := range []string{"arrow.spr", "BUTTMENU_ONLINE_1.S32", "WindCutter.S32"} {
+	for _, name := range []string{"arrow.spr", "BUTTMENU_ONLINE_1.S32", "WindCutter.S32"} {
 		func() {
-			f, err := os.Open(filepath.Join("test", "data", tc))
+			f, err := os.Open(filepath.Join("test", "data", name))
 			if err != nil {
-				t.Fatalf("failed to open sprite file: %v", err)
+				t.Fatalf("sprite %q: failed to open file: %v", name, err)
 			}
 			defer f.Close()
 			sp, err := NewSprite(f)
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("sprite %q: failed to open sprite: %v", name, err)
 			}
 			ao := int64(0)
 			for i := 0; i < sp.FrameCount(); i++ {
 				o, err := sp.frameOffset(i)
 				if err != nil {
-					t.Errorf("faild to get frame #%d's offset: %v", i, err)
+					t.Errorf("sprite %q: faild to get frame #%d's offset: %v", name, i, err)
 				}
 				if o != ao {
-					t.Errorf("frame #%d's offset is incorrect; expected %d, got %d", i, ao, o)
+					t.Errorf("sprite %q: frame #%d's offset is incorrect; expected %d, got %d", name, i, ao, o)
 				}
 				s, err := sp.frameSize(i)
 				if err != nil {
-					t.Errorf("failed to get frame #%d's size: %v", i, err)
+					t.Errorf("sprite %q: failed to get frame #%d's size: %v", name, i, err)
 				}
 				ao += int64(s)
 			}
