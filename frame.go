d05a29da8423e648d1b87fad4147a4f04c97a8d5
Hanjun Kim <hallazzang@gmail.com>
2019-01-31 18:46:52 +0900

    add getter methods for Frame's fields

--- frame.go
+++ frame.go
@@ -16,3 +16,23 @@ type Frame struct {
 func newFrame(sp Sprite, idx int, img image.Image) *Frame {
 	return &Frame{sp, idx, sp.FrameWidth(), sp.FrameHeight(), img}
 }
+
+// Index returns frame's index in sprite.
+func (fr *Frame) Index() int {
+	return fr.idx
+}
+
+// Width returns frame's width, in pixels.
+func (fr *Frame) Width() int {
+	return fr.width
+}
+
+// Height returns frame's height, in pixels.
+func (fr *Frame) Height() int {
+	return fr.height
+}
+
+// Image returns frame's image.
+func (fr *Frame) Image() image.Image {
+	return fr.img
+}
