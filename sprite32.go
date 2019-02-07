47855bf970be2a23e078b26343e2687efa81e7e3
Hanjun Kim <hallazzang@gmail.com>
2019-02-07 19:12:07 +0900

    implement sprite32.Save
    i know it's not elegant, but it works for now

--- sprite32.go
+++ sprite32.go
@@ -2,6 +2,7 @@ package gosang
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/binary"
 	"image"
 	"image/color"
@@ -45,6 +46,63 @@ func (sp *sprite32) HasAlpha() bool {
 }
 
 func (sp *sprite32) Save(w io.Writer) error {
+	header := spriteHeader{
+		Signature:   0xf,
+		FrameWidth:  sp.frameWidth,
+		FrameHeight: sp.frameHeight,
+		FrameCount:  sp.frameCount,
+	}
+	if err := binary.Write(w, binary.LittleEndian, &header); err != nil {
+		return errors.Wrap(err, "failed to write sprite header")
+	}
+	offsets := make([]uint32, sp.frameCount)
+	sizes := make([]uint32, sp.frameCount)
+	if err := advanceWriter(w, 0x4c0-16); err != nil {
+		return errors.Wrap(err, "failed to advance writer")
+	}
+	buf := new(bytes.Buffer)
+	offset := uint32(0)
+	for i := uint32(0); i < sp.frameCount; i++ {
+		if sp.frames[i] == nil {
+			return errors.Errorf("frame #%d is empty", i)
+		}
+		size, err := sp.encodeFrame(buf, int(i))
+		if err != nil {
+			return errors.Wrapf(err, "failed to encode frame #%d", i)
+		}
+		offsets[i] = offset
+		sizes[i] = uint32(size)
+		offset += sizes[i]
+	}
+	if err := binary.Write(w, binary.LittleEndian, offsets); err != nil {
+		return errors.Wrap(err, "failed to write frame offsets")
+	}
+	if err := advanceWriter(w, int(0x970-(0x4c0+4*sp.frameCount))); err != nil {
+		return errors.Wrap(err, "failed to advance writer")
+	}
+	for i := uint32(0); i < sp.frameCount; i++ {
+		if err := binary.Write(w, binary.LittleEndian, sizes[i]/4); err != nil {
+			return errors.Wrap(err, "failed to write encoded frame size")
+		}
+	}
+	if err := advanceWriter(w, int(0xe20-(0x970+4*sp.frameCount))); err != nil {
+		return errors.Wrap(err, "failed to advance writer")
+	}
+	if err := binary.Write(w, binary.LittleEndian, offsets[len(offsets)-1]+sizes[len(sizes)-1]); err != nil {
+		return errors.Wrap(err, "failed to write frame data size")
+	}
+	if err := binary.Write(w, binary.LittleEndian, sp.width); err != nil {
+		return errors.Wrap(err, "failed to write frame width")
+	}
+	if err := binary.Write(w, binary.LittleEndian, sp.height); err != nil {
+		return errors.Wrap(err, "failed to write frame height")
+	}
+	if err := advanceWriter(w, 0xe4c-(0xe20+12)); err != nil {
+		return errors.Wrap(err, "failed to advance writer")
+	}
+	if _, err := buf.WriteTo(w); err != nil {
+		return errors.Wrap(err, "failed to write frame data")
+	}
 	return nil
 }
 
@@ -70,6 +128,71 @@ func (sp *sprite32) loadFrame(idx int) (*Frame, error) {
 		sp.frames[idx] = newFrame(sp, idx, img)
 	}
 	return sp.frames[idx], nil
+}
+
+func (sp *sprite32) encodeFrame(w io.Writer, idx int) (int, error) {
+	if idx < 0 || idx > int(sp.frameCount-1) {
+		return 0, errors.New("frame index out of range")
+	}
+	img := sp.frames[idx].img
+	if img == nil {
+		return 0, errors.Errorf("frame #%d's image is empty", idx)
+	}
+	b := img.Bounds()
+	if b.Empty() {
+		return 0, errors.Errorf("invalid image bounds: %v", b)
+	}
+	width, height := b.Dx(), b.Dy()
+	if uint32(width) != sp.frameWidth || uint32(height) != sp.frameHeight {
+		return 0, errors.New("mismatched frame size")
+	}
+	n := 0
+	for y := 0; y < height; y++ {
+		c := uint8(0)
+		for x := 0; x < width; x++ {
+			r, g, b, _ := rgbaAt(img, x, y)
+			if x+1 != width {
+				tr, tg, tb, _ := rgbaAt(img, x+1, y)
+				if r == tr && g == tg && b == tb && c < 0xff {
+					if x == 0 {
+						c += 2
+					} else if c < 1 {
+						if err := binary.Write(w, binary.LittleEndian, sprite32Pixel{1, b, g, r}); err != nil {
+							return n, errors.Wrap(err, "failed to write frame data")
+						}
+						n += 4
+						c++
+					} else {
+						c++
+					}
+				} else {
+					if c > 0 {
+						if err := binary.Write(w, binary.LittleEndian, sprite32Pixel{c, b, g, r}); err != nil {
+							return n, errors.Wrap(err, "failed to write frame data")
+						}
+					} else {
+						if err := binary.Write(w, binary.LittleEndian, sprite32Pixel{1, b, g, r}); err != nil {
+							return n, errors.Wrap(err, "failed to write frame data")
+						}
+					}
+					n += 4
+					c = 0
+				}
+			} else {
+				if c > 0 {
+					if err := binary.Write(w, binary.LittleEndian, sprite32Pixel{c, b, g, r}); err != nil {
+						return n, errors.Wrap(err, "failed to write frame data")
+					}
+				} else {
+					if err := binary.Write(w, binary.LittleEndian, sprite32Pixel{1, b, g, r}); err != nil {
+						return n, errors.Wrap(err, "failed to write frame data")
+					}
+				}
+				n += 4
+			}
+		}
+	}
+	return n, nil
 }
 
 type sprite32Pixel struct{ Count, Blue, Green, Red uint8 }
